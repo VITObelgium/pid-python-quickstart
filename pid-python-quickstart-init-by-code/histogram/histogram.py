2b3caf43b33cd2f65885e026d5d9add69f89b2ed
Stephane Tonneau <stephane.tonneau@nrb.be>
2018-05-16 17:25:18 +0200

    LOW : better comment

--- pid-python-quickstart-init-by-code/histogram/histogram.py
+++ pid-python-quickstart-init-by-code/histogram/histogram.py
@@ -8,7 +8,7 @@ def histogram(image_info):
     """Calculates the histogram for a given (single band) image file."""
 
     """
-    Initialize the logging system ( which is by default a simple console logger ) and flush the initial log
+    Initialize the logging system with kafka configuration and flush the initial log
     """
     kafka_system=KafkaLogging(brokers="epod1.vgt.vito.be:6668,epod17.vgt.vito.be:6668",topic='pid_test2_es' )
     process_log = logging_factory.LoggingFactory(classes=[kafka_system]).get_logger("-","HISTOGRAM",datetime.datetime.now())
--- pid-python-quickstart-init-by-code/spark.py
+++ pid-python-quickstart-init-by-code/spark.py
@@ -17,7 +17,7 @@ to be executed in parallel.
 """
 if __name__ == '__main__':
     """
-    Initialize the logging system ( which is by default a simple console logger ) and flush the initial log
+    Initialize the logging system with kafka configuration and flush the initial log
     """
     kafka_system=KafkaLogging(brokers="epod1.vgt.vito.be:6668,epod17.vgt.vito.be:6668",topic='pid_test2_es' )
     process_log = logging_factory.LoggingFactory(classes=[kafka_system]).get_logger("-","HISTOGRAM",datetime.now())
